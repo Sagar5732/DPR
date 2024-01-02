575cafd72caf9f8143879744aef14990ebe5a2b7
Sagar5732 <72186190+Sagar5732@users.noreply.github.com>
2024-01-02 18:40:02 +0530

    Add files via upload

---
+++ b2a/src/main/java/com/b2a/demo/B2aApplication.java
@@ -0,0 +1,13 @@
+package com.b2a.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2aApplication.class, args);
+	}
+
+}
