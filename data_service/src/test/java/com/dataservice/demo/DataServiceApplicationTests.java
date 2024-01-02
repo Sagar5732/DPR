1a0bf233f82a955d77705c50a3ed4157afdd8bd6
Sagar <sagarkamble2431992@gmail.com>
2024-01-02 18:35:44 +0530

    1st commit for sagar

---
+++ data_service/src/test/java/com/dataservice/demo/DataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataservice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
