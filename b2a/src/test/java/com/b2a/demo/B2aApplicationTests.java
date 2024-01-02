a7d0ec097af167335ed057588c0f34b3f22e1a89
Sagar <sagarkamble2431992@gmail.com>
2024-01-02 13:00:45 +0530

    1st commit

---
+++ b2a/src/test/java/com/b2a/demo/B2aApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b2a.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
